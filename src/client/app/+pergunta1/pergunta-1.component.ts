ab4fe6801be3398020bea85ba95b6ab7809200f8
Jordan Pena <jordanpena@ongbook.org>
2016-08-29 22:34:32 -0300

    implementando interpolation, property binding e event binding

--- src/client/app/+pergunta1/pergunta-1.component.ts
+++ src/client/app/+pergunta1/pergunta-1.component.ts
@@ -8,4 +8,16 @@ import { Component } from '@angular/core';
 
 })
 
-export class Pergunta1Component{ }
+export class Pergunta1Component{
+  //trazer de um service
+  title: string = 'O que comove você?';
+  urlImg1 = 'https://http.cat/405.jpg';
+  urlImg2 = 'https://http.cat/100.jpg';
+  resp1: number;
+
+  clickLink(value: number){
+    this.resp1 = value;
+    console.log(this.resp1);
+  }
+
+}
